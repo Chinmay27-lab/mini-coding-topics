bd3c201a2de9434ec03392e9929731c67fe6b448
Chinmay27-lab <byterogue16x@gmail.com>
2026-03-27 23:59:34 +0530

    Added Insertion Sort Algorithm

---
+++ Insertion_sort.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+    int n ; 
+    cin >> n ;
+    int arr[n];
+    for(int i = 0 ; i < n ; i++)
+    {
+        cin >> arr[i] ;
+    }
+
+    for(int i = 1 ; i < n ; i++)
+    {
+        int curr = arr[i] ;
+        int prev = i-1 ;
+        while(prev >=0 && arr[prev]>curr)
+        {
+            arr[prev+1] = arr[prev] ;
+            prev--;
+        }
+
+        arr[prev+1] = curr ;
+    }
+
+    cout << "Sorted array : ";
+    for(int i = 0 ; i < n ; i ++)
+    {
+        cout << arr[i] << " " ;
+    }
+}
